e3618e752148346b87945f775517f457dc6ac73b
R D <ryandowney64@yahoo.com>
2023-06-04 23:04:57 +0100

    CDN troubleshooting

--- mysite/Chat_DJ4E/settings.py
+++ mysite/Chat_DJ4E/settings.py
@@ -122,6 +122,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_LOCATION = config('AWS_LOCATION')
+AWS_DEFAULT_ACL = 'public-read'
 
 CHANNEL_LAYERS = {
     "default": {
@@ -173,8 +174,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 TEMP = os.path.join(BASE_DIR, 'temp')
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
