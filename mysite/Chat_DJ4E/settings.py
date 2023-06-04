300215c1efb747d48f398df8b1a92ae6a24d663f
R D <ryandowney64@yahoo.com>
2023-06-04 23:23:56 +0100

    CDN troubleshooting pt2

--- mysite/Chat_DJ4E/settings.py
+++ mysite/Chat_DJ4E/settings.py
@@ -174,7 +174,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 TEMP = os.path.join(BASE_DIR, 'temp')
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
