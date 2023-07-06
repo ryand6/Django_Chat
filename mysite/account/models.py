d4ff8288a099821b3d61061971e528fc6e9ca77e
R D <ryandowney64@yahoo.com>
2023-07-06 21:41:05 +0100

    mobile version, admin site and code snippet fixes as well as visual tweaks and reorganisation

--- mysite/account/models.py
+++ mysite/account/models.py
@@ -48,7 +48,7 @@ def get_default_image_path():
 
 class Account(AbstractBaseUser):
     email = models.EmailField(verbose_name="email", max_length=255, unique=True)
-    username = models.CharField(max_length=30, unique=True)
+    username = models.CharField(max_length=25, unique=True)
     date_joined = models.DateTimeField(verbose_name="date joined", auto_now_add=True)
     last_login = models.DateTimeField(verbose_name="last login", auto_now=True)
     is_admin = models.BooleanField(default=False)
